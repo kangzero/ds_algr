265e8ac083a01cc8d65ef5faa65ab1836143a628
nkang <kangzero@gmail.com>
2019-03-24 01:09:00 -0700

    doubly linkedlist init

---
+++ linear/doubly_linkedlist/doubly_linkedlist.c
@@ -0,0 +1,23 @@
+/***************************************************
+ *
+ * Filename: doubly_linkedlist.c
+ *
+ * Version: v1.0
+ * History:
+ * V1.0     Created by Ning kang    03/24/2019
+ *
+ * Copyright(c) 2019 by Ning Kang
+ * This software is placed in the public domain
+ * and may be used for any purpose. However, this
+ * notice must not be changed or removed and no
+ * warranty is either expressed or implied by its
+ * publician or distribution.
+ *
+ **************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include "doubly_linkedlist.h"
+
+
