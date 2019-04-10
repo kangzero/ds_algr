6781c05161f970f4ce76327dcad25025725037b1
nkang <kangzero@gmail.com>
2019-04-09 20:07:26 -0700

    add makefile & basic list algorithm

---
+++ linear/singly_linkedlist/singly_linkedlist_op.c
@@ -0,0 +1,56 @@
+/********************************************************************************
+ *
+ * Filename: singly_linkedlist_op.c
+ * Description: singly linkedlist operation implementation by C
+ *
+ * Version: v1.0
+ * History:
+ * V1.0     Created by Ning Kang     04/09/2019
+ *
+ * Copyright(c) 2019 by Ning Kang
+ *
+ * This software is placed into the public domain and may be used for any purpose.
+ * However, this notice must not be changed or removed and no warranty is either
+ * expressed or implied by its publication or distribution.
+ *
+ ********************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "singly_linkedlist.h"
+#include "singly_linkedlist_op.h"
+
+#define RECURSIVE 1
+#define ITERATIVE 0
+
+//reverse list
+ListNode* linkedlist_reverse(ListNode *head)
+{
+    if ((head == NULL) || (head->next == NULL))
+    {
+        return head;
+    }
+
+#if RECURSIVE
+    ListNode *p = linkedlist_reverse(head->next);
+    head->next->next = head;
+    head->next = NULL;
+
+    return p;
+#elif ITERATIVE
+    ListNode *cur = head;
+    ListNode *pre = NULL;
+
+    while (cur != NULL)
+    {
+        ListNode *next = cur->next;
+        cur->next = pre; //reverse happens here
+        pre = cur;
+        cur = next;
+    }
+
+    return pre;
+#endif
+}
+
+
---
+++ linear/singly_linkedlist/singly_linkedlist_op.h
@@ -0,0 +1,23 @@
+/********************************************************************************
+ *
+ * Filename: singly_linkedlist_op.h
+ * Description: singly linkedlist operation head file
+ *
+ * Version: v1.0
+ * History:
+ * V1.0     Created by Ning Kang     04/09/2019
+ *
+ * Copyright(c) 2019 by Ning Kang
+ *
+ * This software is placed into the public domain and may be used for any purpose.
+ * However, this notice must not be changed or removed and no warranty is either
+ * expressed or implied by its publication or distribution.
+ *
+ ********************************************************************************/
+
+#ifndef SINGLY_LINKEDLIST_OP_H
+#define SINGLY_LINKEDLIST_OP_H
+
+ListNode* linkedlist_reverse(ListNode *head);
+
+#endif
